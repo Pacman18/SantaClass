850b207e376c97102f10efa733a1945e9c095036
pacman18 <lado@nate.com>
2024-06-12 23:54:30 +0900

    c# 프로젝트 추가

---
+++ Sharp/WeekEndClass/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 컴퓨터한테 알려준다. 
+string name = "이매산타";
+
+var cl = new TestClass();
+
+cl.Shout();
