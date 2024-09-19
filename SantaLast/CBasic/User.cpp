6dd0322b71976da0959f58f64e4ff6a25d1b7264
pacman18 <lado@nate.com>
2024-09-19 23:50:11 +0900

    C++ 기초 추가

---
+++ SantaLast/CBasic/User.cpp
@@ -0,0 +1,10 @@
+
+#include "User.h"
+#include<iostream>
+
+
+void User::ShowLog()
+{
+	std::cout << "로그로그" << std::endl;
+
+}
---
+++ SantaLast/CBasic/User.h
@@ -0,0 +1,11 @@
+#pragma once
+class User
+{
+public :
+	void ShowLog();
+
+private :
+	void ShowPivateLog();
+
+};
+
